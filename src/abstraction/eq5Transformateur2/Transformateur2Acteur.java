6b43950ebef5b5b1594eaea812dd1a082ecbebbe
Pierre Guttierez <pierre.guttierez@imt-atlantique.net>
2026-04-07 08:47:46 +0200

    Vendeur CC

--- src/abstraction/eq5Transformateur2/Transformateur2Acteur.java
+++ src/abstraction/eq5Transformateur2/Transformateur2Acteur.java
@@ -34,6 +34,7 @@ public class Transformateur2Acteur implements IActeur {
 		this.Journaux.add(new Journal("Stock Feves", this));
 		this.Journaux.add(new Journal("Stock Chocolat", this));
 		this.Journaux.add(new Journal("Achat Contrat Cadre", this));
+		this.Journaux.add(new Journal("Vente Contrat Cadre", this));
 		this.Journaux.add(new Journal("Achat Enchère", this));
 		this.Journaux.add(new Journal("Achat Bourse", this));
 		this.Journaux.add(new Journal("Vente AO", this));
@@ -65,7 +66,7 @@ public class Transformateur2Acteur implements IActeur {
 	/** @author Pierre
 	 */
 	public void next() {
-		for (int i = 0; i < 8; i++){
+		for (int i = 0; i < 9; i++){
 			this.Journaux.get(i).ajouter("Etape : "+ Integer.toString((Filiere.LA_FILIERE.getEtape()))+ "\n");
 		}
 	}
--- src/abstraction/eq5Transformateur2/Transformateur2VendeurCC.java
+++ src/abstraction/eq5Transformateur2/Transformateur2VendeurCC.java
@@ -17,8 +17,9 @@ import abstraction.eqXRomu.bourseCacao.BourseCacao;
 
 /**
  * @author Pierre GUTTIEREZ
+ */
  
-public class Transformateur2VendeurCC extends Transformateur2AcheteurBourse implements IVendeurContratCadre{
+public class Transformateur2VendeurCC extends Transformateur2ProductionChocolat implements IVendeurContratCadre{
 
     public Transformateur2VendeurCC() {
         super();
@@ -43,14 +44,39 @@ public class Transformateur2VendeurCC extends Transformateur2AcheteurBourse impl
 	}
 	
 	public double propositionPrix(ExemplaireContratCadre contrat){
-
+		return 1.35*contrat.getQuantiteTotale()*(prix_MP+2);
 	}
 
-	public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat);
+	public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat){
+		return ((contrat.getPrix() + contrat.getListePrix().get(contrat.getListePrix().size() - 2))/2)*1.20;
+	}
 	
 
-	public void notificationNouveauContratCadre(ExemplaireContratCadre contrat);
+	public void notificationNouveauContratCadre(ExemplaireContratCadre contrat){
+		this.getJournaux().get(4).ajouter(contrat.toString()+ "\n");
+	}
 
-	public double livrer(IProduit produit, double quantite, ExemplaireContratCadre contrat);
+	public double livrer(IProduit produit, double quantite, ExemplaireContratCadre contrat){
+		if (produit instanceof Chocolat){
+			Chocolat c = (Chocolat) produit;
+			Feve F;
+			if (c == Chocolat.C_BQ){
+				F = Feve.F_BQ;
+			} if (c == Chocolat.C_MQ) {
+				F = Feve.F_MQ;
+			} else {
+				F = Feve.F_HQ;
+			}
+			if (this.getStock_chocolat(produit) >= quantite){
+				this.remove_chocolat(quantite, c);
+				return quantite;
+			} else {
+				this.ProductionChocolat(F,0.45,quantite - this.getStock_chocolat(produit));
+			this.remove_chocolat(this.getStock_chocolat(produit), c);
+			return quantite;
+			}
+		} else {
+			return 0;
+		}
+	}
 }
-*/
