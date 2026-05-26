d5fda06e52863c5d3f64a5d475d0465c0fb70f66
Pol Bailleul <polbailleul@gmail.com>
2026-05-26 18:56:13 +0200

    limites

--- src/abstraction/eq6Transformateur3/Transformateur3AcheteurCCadre.java
+++ src/abstraction/eq6Transformateur3/Transformateur3AcheteurCCadre.java
@@ -42,27 +42,34 @@ public class Transformateur3AcheteurCCadre extends Transformateur3AcheteurBourse
 		this.journalCC.ajouter("Etape"+Filiere.LA_FILIERE.getEtape());
 			for (Feve f : stockFeve.getFeves()) {
 				if (f == Feve.F_HQ_E || f == Feve.F_MQ_E ) {
-					double besoin = 21200 - stockFeve.getQuantite(f) - restantDu(f);
-					if (besoin > 0) {
-						this.journalCC.ajouter("   "+f+" suffisamment peu en stock/contrat pour passer un CC");
-						double parStep = Math.max(100, (21200-stockFeve.getQuantite(f)-restantDu(f))/12); // au moins 100
-						Echeancier e = new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 12, parStep);
-						List<IVendeurContratCadre> vendeurs = supCC.getVendeurs(f);
-						if (vendeurs.size()>0) {
-							IVendeurContratCadre vendeur = vendeurs.get(Filiere.random.nextInt(vendeurs.size()));
-							journalCC.ajouter("   "+vendeur.getNom()+" retenu comme vendeur parmi "+vendeurs.size()+" vendeurs potentiels");
-							ExemplaireContratCadre contrat = supCC.demandeAcheteur(this, vendeur, f, e, cryptogramme, false);
-							if (contrat==null) {
-								journalCC.ajouter(Color.RED, Color.white,"   echec des negociations");
+					double stockChocoAssocie = 0;
+					if (f == Feve.F_HQ_E) stockChocoAssocie = this.getStockProduit(LamborghiniduCacao);
+					if (f == Feve.F_MQ_E) stockChocoAssocie = this.getStockProduit(Chocoenbien);
+					if (stockChocoAssocie<22000){
+						double besoin = 21200 - stockFeve.getQuantite(f) - restantDu(f);
+						if (besoin > 0) {
+							this.journalCC.ajouter("   "+f+" suffisamment peu en stock/contrat pour passer un CC");
+							double parStep = Math.max(100, besoin/12); // au moins 100
+							Echeancier e = new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 12, parStep);
+							List<IVendeurContratCadre> vendeurs = supCC.getVendeurs(f);
+							if (vendeurs.size()>0) {
+								IVendeurContratCadre vendeur = vendeurs.get(Filiere.random.nextInt(vendeurs.size()));
+								journalCC.ajouter("   "+vendeur.getNom()+" retenu comme vendeur parmi "+vendeurs.size()+" vendeurs potentiels");
+								ExemplaireContratCadre contrat = supCC.demandeAcheteur(this, vendeur, f, e, cryptogramme, false);
+								if (contrat==null) {
+									journalCC.ajouter(Color.RED, Color.white,"   echec des negociations");
+								} else {
+									this.contratsEnCours.add(contrat);
+									journalCC.ajouter(Color.GREEN, vendeur.getColor(), "   contrat signe");
+								}
 							} else {
-								this.contratsEnCours.add(contrat);
-								journalCC.ajouter(Color.GREEN, vendeur.getColor(), "   contrat signe");
+								journalCC.ajouter("   pas de vendeur");
 							}
-						} else {
-							journalCC.ajouter("   pas de vendeur");
 						}
 					}
-				}
+					else {
+						this.journalCC.ajouter("   Achats CC suspendus pour " + f + " : Surstock de chocolat (" + stockChocoAssocie + " T)");
+					}
 			}
 		// On archive les contrats termines
 		for (ExemplaireContratCadre c : this.contratsEnCours) {
@@ -78,6 +85,7 @@ public class Transformateur3AcheteurCCadre extends Transformateur3AcheteurBourse
         int etape = Filiere.LA_FILIERE.getEtape();
         journalCC.ajouter("Etape"+ etape);
 	}
+}
 
 	public double restantDu(Feve f) {
 		double res=0;
